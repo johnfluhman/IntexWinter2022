a94e493ed63746945b2221eccbb9c954374ea4fd
yejinchoi115 <yjjjang115@gmail.com>
2022-04-05 17:14:31 -0600

    yejin-onnx model

--- Intex2/Intex2/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Admin/_Imports.razor.g.cs
+++ Intex2/Intex2/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Admin/_Imports.razor.g.cs
@@ -12,42 +12,42 @@ namespace Intex2.Pages.Admin
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 1 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 2 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 3 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 4 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 5 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/johnfluhman/Documents/GitHub/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
+#line 6 "/Users/yejinchoi/Projects/IntexWinter2022/Intex2/Intex2/Pages/Admin/_Imports.razor"
 using Intex2.Models;
 
 #line default
